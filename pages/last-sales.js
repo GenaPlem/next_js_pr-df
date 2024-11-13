14ce53f46139fca8c8091d0efe788dabf00dfad1
GenaPlem <shainkill1337@gmail.com>
2024-11-13 12:31:14 +0000

    Add data pre-fetching with getStaticProps function

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-export default function LastSalesPage() {
-  const [sales, setSales] = useState();
+export default function LastSalesPage(props) {
+  const [sales, setSales] = useState(props.sales);
   // const [isLoading, setIsLoading] = useState(false);
 
   const { data, error } = useSWR(
@@ -52,7 +52,7 @@ export default function LastSalesPage() {
     return <p>Failed to load!</p>;
   }
 
-  if (!data || !sales) {
+  if (!data && !sales) {
     return <p>Loading...</p>;
   }
 
@@ -65,4 +65,23 @@ export default function LastSalesPage() {
       ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://next-js-course-a24fe-default-rtdb.europe-west1.firebasedatabase.app/sales.json"
+  );
+  const data = await response.json();
+
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  return { props: { sales: transformedSales }, revalidate: 10 };
 }
